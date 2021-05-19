4aa55f10c4ce564027382c970024318c19dd9928
adi480 <83358373+adi480@users.noreply.github.com>
2021-05-19 21:01:01 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,48 @@
+class Player
+{
+    constructor() {
+		this.x = 500;
+		this.y=200;
+		this.spt=createSprite(this.x, this.y, 100,100);
+		this.spt.shapeColor="blue";
+
+	}
+
+  applyGravity()
+    {
+        this.spt.velocityY=this.spt.velocityY+2;
+	}
+	moveLeft(){
+this.spt.x=this.spt.x-20;
+	}
+	moveRight(){
+		this.spt.x=this.spt.x+20;
+	}
+	jump(){
+this.spt.velocityY=-20;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
